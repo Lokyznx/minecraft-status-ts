895c1a1a01ebd85e11db7fdda7a05a28db6e2898
Taylon <116691365+Lokyznx@users.noreply.github.com>
2025-06-19 11:57:35 -0300

    Delete src/protocols/index.ts

--- src/protocols/index.ts
+++
@@ -1,5 +0,0 @@
-export { queryJavaServer, MinecraftQueryError as JavaQueryError } from './protocols/java/queryJavaServer';
-export { queryBedrockServer, MinecraftQueryError as BedrockQueryError } from './protocols/bedrock/queryBedrockServer';
-
-export type { JavaServerStatus } from './protocols/java/types';
-export type { BedrockServerStatus } from './protocols/bedrock/types';
