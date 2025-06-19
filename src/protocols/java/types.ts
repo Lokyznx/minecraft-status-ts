8b16942fa77c143953d9ca68a9bab6ac2c2773c2
Taylon <116691365+Lokyznx@users.noreply.github.com>
2025-06-19 10:32:03 -0300

    Update types.ts

--- src/protocols/java/types.ts
+++ src/protocols/java/types.ts
@@ -1,1 +1,18 @@
+export interface JavaServerStatus {
+  online: boolean;
+  host: string;
+  port: number;
+  version?: string;
+  playersOnline?: number;
+  playersMax?: number;
+  description?: string;
+  favicon?: string | null;
+  latency?: number;
+}
 
+export interface MinecraftJavaStatusResponse {
+  version: { name: string };
+  players: { online: number; max: number };
+  description: string | { text?: string; extra?: { text?: string }[] };
+  favicon?: string;
+}
