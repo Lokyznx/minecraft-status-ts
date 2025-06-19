132723a5ce57a9546b684458798e9d3e5823f513
Taylon <116691365+Lokyznx@users.noreply.github.com>
2025-06-19 11:41:47 -0300

    Update types.ts

--- src/protocols/bedrock/types.ts
+++ src/protocols/bedrock/types.ts
@@ -1,1 +1,14 @@
-
+export interface BedrockServerStatus {
+  online: boolean;
+  host: string;
+  port: number;
+  edition?: string;
+  motd?: string;
+  version?: string;
+  protocol?: number;
+  playersOnline?: number;
+  playersMax?: number;
+  serverId?: string;
+  gameMode?: string;
+  latency?: number;
+}
